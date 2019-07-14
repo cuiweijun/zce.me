08d3c1d2c7ebbe7c4fa1d91c54615475bafad0e9
zce <w@zce.me>
2019-07-14 16:25:05 +0800

    feat: initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -95,7 +95,8 @@ const createMarkdownFields = ({ node, getNode, actions }) => {
   date = new Date(date)
   updated = new Date(updated)
 
-  authors.length || authors.push('Gatsby')
+  // TODO: change default author
+  authors.length || authors.push('Lei Wang')
   categories.length || categories.push('Uncategorized')
   tags.length || tags.push('Untagged')
 
@@ -163,104 +164,99 @@ exports.onCreateNode = args => {
   }
 }
 
-// https://www.gatsbyjs.org/docs/creating-and-modifying-pages/
-exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ graphql, actions, reporter }) => {
+//   // https://www.gatsbyjs.org/docs/creating-and-modifying-pages/
+//   const { createPage } = actions
 
-  const result = await graphql(`
-    query {
-      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
-        edges {
-          node {
-            id
-            fields {
-              type
-              template
-              permalink
-            }
-            frontmatter {
-              title
-              date
-            }
-          }
-        }
-      }
-      allAuthorsYaml {
-        edges {
-          node {
-            slug
-            fields {
-              type
-              template
-              permalink
-            }
-          }
-        }
-      }
-      allCategoriesYaml {
-        edges {
-          node {
-            slug
-            fields {
-              type
-              template
-              permalink
-            }
-          }
-        }
-      }
-      allTagsYaml {
-        edges {
-          node {
-            slug
-            fields {
-              type
-              template
-              permalink
-            }
-          }
-        }
-      }
-    }
-  `)
+//   const result = await graphql(`
+//     query {
+//       allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+//         edges {
+//           node {
+//             id
+//             fields {
+//               title
+//               date
+//               type
+//               template
+//               permalink
+//             }
+//           }
+//         }
+//       }
+//       allAuthorsYaml {
+//         edges {
+//           node {
+//             fields {
+//               type
+//               template
+//               permalink
+//             }
+//           }
+//         }
+//       }
+//       allCategoriesYaml {
+//         edges {
+//           node {
+//             fields {
+//               type
+//               template
+//               permalink
+//             }
+//           }
+//         }
+//       }
+//       allTagsYaml {
+//         edges {
+//           node {
+//             fields {
+//               type
+//               template
+//               permalink
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  // errors report
-  result.errors && reporter.panic(result.errors)
+//   // errors report
+//   result.errors && reporter.panic(result.errors)
 
-  const { edges: posts } = result.data.allMarkdownRemark
+//   const { edges: posts } = result.data.allMarkdownRemark
 
-  // https://www.gatsbyjs.org/docs/adding-pagination/
-  // Create pages based on different content types
-  Object.values(collections)
-    .map(c => c.type)
-    .forEach(type => {
-      const items = posts.filter(e => e.node.fields.type === type)
-      items.forEach(({ node: { id, fields } }, i) => {
-        const prev = i === items.length - 1 ? null : items[i + 1].node
-        const next = i === 0 ? null : items[i - 1].node
-        const template = `./src/templates/${fields.template}.js`
-        createPage({
-          path: fields.permalink,
-          component: require.resolve(template),
-          context: { id, prev, next }
-        })
-      })
-    })
+//   // https://www.gatsbyjs.org/docs/adding-pagination/
+//   // Create pages based on different content types
+//   Object.values(collections)
+//     .map(c => c.type)
+//     .forEach(type => {
+//       const items = posts.filter(e => e.node.fields.type === type)
+//       items.forEach(({ node: { id, fields } }, i) => {
+//         const prev = i === items.length - 1 ? null : items[i + 1].node
+//         const next = i === 0 ? null : items[i - 1].node
+//         const template = `./src/templates/${fields.template}.js`
+//         createPage({
+//           path: fields.permalink,
+//           component: require.resolve(template),
+//           context: { id, prev, next }
+//         })
+//       })
+//     })
 
-  // https://www.gatsbyjs.org/docs/adding-tags-and-categories-to-blog-posts/
-  const { edges: authors } = result.data.allAuthorsYaml
-  const { edges: categories } = result.data.allCategoriesYaml
-  const { edges: tags } = result.data.allTagsYaml
-  const meta = [].concat(authors, categories, tags)
+//   // https://www.gatsbyjs.org/docs/adding-tags-and-categories-to-blog-posts/
+//   const { edges: authors } = result.data.allAuthorsYaml
+//   const { edges: categories } = result.data.allCategoriesYaml
+//   const { edges: tags } = result.data.allTagsYaml
+//   const meta = [].concat(authors, categories, tags)
 
-  // Create taxonomies pages
-  meta.forEach(item => {
-    const { slug, fields } = item.node
-    const template = `./src/templates/${fields.template}.js`
-    createPage({
-      path: fields.permalink,
-      component: require.resolve(template),
-      context: { slug }
-    })
-  })
-}
+//   // Create taxonomies pages
+//   meta.forEach(item => {
+//     const { slug, fields } = item.node
+//     const template = `./src/templates/${fields.template}.js`
+//     createPage({
+//       path: fields.permalink,
+//       component: require.resolve(template),
+//       context: { slug }
+//     })
+//   })
+// }

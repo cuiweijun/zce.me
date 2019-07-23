fe04b0fb3f317993c8a63af8649358618fa14836
zce <w@zce.me>
2019-07-23 17:25:42 +0800

    feat: footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default ({ author }) => (
   <footer className="site-footer">
-    <p className="container mb-0 py-4 text-center text-muted">
+    <p className="container">
       &copy; {new Date().getFullYear()} by {author}, Built with{' '}
       <a href="https://gatsbyjs.org" target="_blank" rel="noopener noreferrer">
         Gatsby

61d6052a739a2fcfd411b7350a1cf3037b3101b1
zce <w@zce.me>
2019-07-23 17:21:34 +0800

    feat: header

--- src/components/header.js
+++ src/components/header.js
@@ -4,29 +4,19 @@ import { Link } from 'gatsby'
 export default ({ title, logo, menus }) => {
   return (
     <header className="site-header">
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            <img
-              className="align-top mr-2"
-              alt={title}
-              src={logo}
-              width="30"
-              height="30"
-            />
-            <span>{title}</span>
-          </Link>
-          <ul className="navbar-nav">
-            {menus.map(i => (
-              <li className="nav-item" key={i.link}>
-                <Link className="nav-link" activeClassName="active" to={i.link}>
-                  {i.text}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
+      <div className="container">
+        <Link className="site-brand" to="/">
+          <img alt={title} src={logo} width="25" height="25" />
+          <span>{title}</span>
+        </Link>
+        <nav className="site-nav">
+          {menus.map(i => (
+            <Link to={i.link} key={i.link}>
+              {i.text}
+            </Link>
+          ))}
+        </nav>
+      </div>
     </header>
   )
 }

7b988adb4d37a30ebe674ae59be2eea6a0249fd6
zce <w@zce.me>
2020-02-24 16:02:07 +0800

    feat: 404 animation

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { keyframes } from '@emotion/core'
 
 import {
   Layout,
@@ -11,11 +12,84 @@ import {
   Card
 } from '../components'
 
+const move = keyframes`
+  10% {
+    top: -0.8px;
+    left: -2.2px;
+  }
+  20% {
+    filter: hue-rotate(-90deg);
+    top: 0.8px;
+    left: -0.4px;
+  }
+  30% {
+    filter: hue-rotate(0);
+    left: 1px;
+  }
+  40% {
+    top: -0.6px;
+    left: -1.4px;
+  }
+  50% {
+    filter: blur(1px);
+    left: 0.4px;
+  }
+  60% {
+    filter: blur(0);
+    top: 3.6px;
+    left: -2.4px;
+  }
+  70% {
+    top: -2px;
+    left: 0.2px;
+  }
+  80% {
+    top: -0.8px;
+    left: -1.8px;
+  }
+  90% {
+    left: 2.4px;
+  }
+  100% {
+    left: -2.4px;
+  }
+`
+
+const Heading = () => (
+  <h1
+    sx={{
+      display: 'inline',
+      position: 'relative',
+      fontSize: '8rem',
+      color: 'transparent',
+      ':before': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        content: '"404"',
+        color: 'red',
+        mixBlendMode: 'lighten',
+        animation: `${move} 0.95s infinite`
+      },
+      ':after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        content: '"404"',
+        color: 'cyan',
+        mixBlendMode: 'lighten',
+        animation: `${move} 2.2s infinite -0.5s`
+      }
+    }}>
+    404
+  </h1>
+)
+
 export default ({ data: { posts } }) => (
   <Layout title="404 Not found">
     <Cover type={3} />
     <Hero sx={{ py: '12vw' }}>
-      <h1 sx={{ fontSize: '8rem', fontWeight: 'light' }}>404</h1>
+      <Heading />
       <p sx={{ mb: 6 }}>你似乎来到了新大陆～</p>
       <Button as={Link} to="/">
         返回首页 <span aria-hidden="true">&rarr;</span>
